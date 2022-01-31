81e1b89be0b74585d3ab8fd2d5a96620fc6e8c79
Anurag Mishra <am91294@gmail.com>
2022-01-31 16:45:07 +0530

    Create MaxAndMinIntegers.java

---
+++ src/com/array/programs/MaxAndMinIntegers.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+  
+public class MaxAndMinIntegers {
+    public static void main(String[] args){
+        int arr[] = {10, 324, 45, 90, 9808};
+        int max = Arrays.stream(arr).max().getAsInt();
+        int min = Arrays.stream(arr).min().getAsInt();
+        System.out.println("Largest in given array is " +max);
+        System.out.println("Smallest in given array is " +min);
+    }
+  
+}
